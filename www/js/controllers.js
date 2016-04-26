f9d96989dda002485849e875e705e409bb40c763
Talex <yoloswaggins@originaljokes.com>
2016-04-26 15:57:57 +0100

    Added extra fields to register.html and added authData setters for the new data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,10 +2,10 @@ angular.module('starter.controllers', [])
 
 
 .controller('LoginCtrl', function($scope, $firebaseAuth, $location, $state) {
-    $scope.login = function(username, password) {
+    $scope.login = function(email, password) {
         var fbAuth = $firebaseAuth(fb);
         fbAuth.$authWithPassword({
-            email: username,
+            email: email,
             password: password
         }).then(function(authData) {
             $location.path("/tab/profile");
@@ -14,8 +14,8 @@ angular.module('starter.controllers', [])
         });
     }
 
-    $scope.register = function(username, password) {
-	    $state.go("tab.register", { user: username, pass: password })
+    $scope.register = function(email, password) {
+	    $state.go("tab.register", { mail: email, pass: password })
     }
 })
 
@@ -25,25 +25,49 @@ angular.module('starter.controllers', [])
 	with ($scope) {
 		if (password != confirmPassword)
 			throw "Passwords don't match"
-		fbAuth.$createUser({email: username, password: password}).then(function() {
+		fbAuth.$createUser({email: email, password: password}).then(function() {
 		    return fbAuth.$authWithPassword({
-			email: username,
+			email: email,
 			password: password
 		    });
 		}).then(function(authData) {
+
+			//$scope.firstName = $stateParams['fName']
+			//$scope.secondName = $stateParams['sName']
+			var user = fb.child("users").child(authData.uid);
+			user.child("Email").set(email);
+			user.child("First Name").set(firstName);
+			user.child("Second Name").set(secondName);
+			
+			
+			user.once("value", function(snapshot) {
+			console.log(snapshot.val());
+			});
+
 		    $location.path("/tab/profile");
+			
 		}).catch(function(error) {
 		    console.error("ERROR " + error);
 		});
 	}
     }
-    $scope.username = $stateParams['user']
-    $scope.password = $stateParams['pass']
-    $scope.confirmPassword = "";
-    $scope.form = {
-	    username:
+	$scope.email = $stateParams['mail']
+	$scope.password = $stateParams['pass']
+	$scope.firstName = "";
+	$scope.secondName = "";
+	$scope.confirmPassword = "";
+	$scope.form = {
+	    email:
 		    function (value) {
-			    return arguments.length ? $scope.username = value : $scope.username
+			    return arguments.length ? $scope.email = value : $scope.email
+		    },
+	    firstName:
+		    function (value) {
+			    return arguments.length ? $scope.firstName = value : $scope.firstName
+		    },
+	    secondName:
+		    function (value) {
+			    return arguments.length ? $scope.secondName = value : $scope.secondName
 		    },
 	    password:
 		    function (value) {
